d211dda282d0bb86abdec717f4e8d9c902f67ec5
mac0aryan <121402580+mac0aryan@users.noreply.github.com>
2025-09-29 12:27:14 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,18 @@
 const mongoose= require('mongoose');
+const MONGO_URI= "mongodb+srv://support_db_user:XtMkAJTMCR72CD5j@cluster0.emgfnar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const dbConnect=async()=>{
+    try {
+    // Use the connection string from MongoDB Atlas
+    // Example: mongodb+srv://<username>:<password>@cluster0.mongodb.net/Kidorise
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-const dbConnect=()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/Kidorise');
-    console.log("connected top db");
+    console.log('✅ Connected to MongoDB Atlas');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1); // Exit if unable to connect
+  }
 }
 module.exports=dbConnect;
